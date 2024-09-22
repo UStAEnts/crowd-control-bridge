90a413e9355afc494e3226d6e5260085ae87396b
Ryan Delaney <vitineth@gmail.com>
2024-09-22 14:25:11 +0100

    Update config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-var config = {
+const config = {
   lightingInterface: "172.18.0.13",
   crowdcontrolServer: "http://127.0.0.1:3001",
-};
+} as const;
 
-module.exports = config;
+export default config;
