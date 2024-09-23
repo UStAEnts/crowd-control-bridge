55dd622c21e4795d3d53910298ff261b217c461a
Ryan Delaney <vitineth@gmail.com>
2024-09-23 01:14:03 +0100

    Add inverse sin function
    This plots the exact opposite of the sin function, good for running effect opposite each other

--- test/animators/sin.test.ts
+++ test/animators/sin.test.ts
@@ -1,9 +1,19 @@
-import sin from "../../src/animators/sin";
+import { sin, inverseSin } from "../../src/animators/sin";
 import { expect, test } from "vitest";
 
 test("sin animates between 0 and 100 only for 0-255", () => {
   for (let i = 0; i < 255; i++) {
     const result = sin(i);
+    console.log(`${i} = ${result}`);
+    expect(result).toBeLessThanOrEqual(255);
+    expect(result).toBeGreaterThanOrEqual(0);
+  }
+});
+
+test("isin animates between 0 and 100 only for 0-255", () => {
+  for (let i = 0; i < 255; i++) {
+    const result = inverseSin(i);
+    console.log(`${i} = ${result}`);
     expect(result).toBeLessThanOrEqual(255);
     expect(result).toBeGreaterThanOrEqual(0);
   }
